7cd9a776f35ff8c721e3ed52d43440e3547757f7
murakami <ek.h1.hy@gmail.com>
2022-08-08 23:32:37 +0900

    borderをつける

--- pages/centered_above_or_below.js
+++ pages/centered_above_or_below.js
@@ -6,9 +6,9 @@ export default function CenteredAboveOrBelow() {
       <Spacer />
 
       <div className="border container mx-auto h-80 flex flex-col justify-center">
-        <p>Hello</p>
-        <p>Hello</p>
-        <p>Hello</p>
+        <p className="border">Hello</p>
+        <p className="border">Hello</p>
+        <p className="border">Hello</p>
       </div>
     </>
   );
