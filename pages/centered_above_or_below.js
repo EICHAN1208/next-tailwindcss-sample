ae51f03b368bdb75560e9e5cabd271af653392a0
murakami <ek.h1.hy@gmail.com>
2022-08-08 23:25:42 +0900

    上下中央寄せ

---
+++ pages/centered_above_or_below.js
@@ -0,0 +1,15 @@
+import Spacer from "./spacer";
+
+export default function CenteredAboveOrBelow() {
+  return (
+    <>
+      <Spacer />
+
+      <div className="border container mx-auto h-80 flex flex-col justify-center">
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+}
